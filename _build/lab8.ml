59bbc2dfac0f2f6901c9de7bdddeaab761fbf0fe
Michelle Gao <michellegao@Michelles-MacBook-Air-5.local>
2018-04-12 14:16:07 -0400

    lab

--- _build/lab8.ml
+++ _build/lab8.ml
@@ -215,7 +215,7 @@ Exercise 8: Create a new event called publish to signal that all
 stories should be published. The event should be a unit WEvent.event.
 ......................................................................*)
 
-let publish = new_event ;; 
+let publish = new_event () ;; 
 
 (*......................................................................
 Exercise 9: Write a function receive_report to handle new news
